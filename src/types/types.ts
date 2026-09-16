3dd781b3d4f3660edd63197220a1c5200761f492
javiergp23 <javgon2306@gmail.com>
2026-09-16 16:46:01 -0300

    add interfaces tokens

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+export interface AuthResponse {
+    body: {
+        user: User;
+        accessToken: string;
+        refreshToken: string;
+
+    };
+}
+
+export interface AuthResponseError {
+    body:{
+        error: string;
+    };
+}
