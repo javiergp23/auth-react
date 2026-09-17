31e2b1eb7d40b7e09e052b80b917b6560beb2e96
javiergp23 <javgon2306@gmail.com>
2026-09-17 20:09:10 -0300

    update login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,20 +1,53 @@
 import { useState } from "react";
 import {DefaultLayout} from "../layout/DefaultLayout.tsx";
 import { useAuth } from "../auth/AuthProvider.tsx";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { API_URL } from "../auth/constants.ts";
+import type { AuthResponseError } from "../types/types.ts";
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errorResponse, setErrorResponse] = useState("");
     const auth = useAuth();
+    const goTo = useNavigate();
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+          const response = await fetch(`${API_URL}/signup`, {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              name,
+              username,
+              password,
+            }),
+          });
+          if (response.ok) {
+            console.log("User created successfully");
+            setErrorResponse("");
+            goTo("/");
+          } else {
+            console.log("Something went wrong");
+            const json = (await response.json()) as AuthResponseError;
+            setErrorResponse(json.body.error);
+          }
+        } catch (error) {
+          console.error("Error during signup:", error);
+        }
+      }
 
     if(auth.isAuthenticated){
         return <Navigate to="/dashboard"/>
     }
   return (
     <DefaultLayout>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h1>Login</h1>
+        {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
         <label htmlFor="username">Username</label>
         <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
         <label htmlFor="password">Password</label>
